72734e6567ce30784ca25269742e48a79cdc6bad
dmathisen <dan@danmathisen.com>
2020-01-26 18:49:04 -0500

    Code cleanup - create skills component

---
+++ src/components/skills.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Container, Row, Col } from "react-materialize";
+import { useStaticQuery, graphql } from "gatsby";
+
+import Card from "../components/card"
+
+export default () => {
+    const data = useStaticQuery(graphql`
+		query {
+			site {
+				siteMetadata {
+					technologies {
+						name
+						type
+						image
+					}
+				}
+			}
+		}
+	`);
+	
+	const technologies = data.site.siteMetadata.technologies;
+	const languages = technologies.filter(item => item.type === 'language');
+	const libraries = technologies.filter(item => item.type === 'library');
+    const others = technologies.filter(item => item.type === 'other');
+    
+    return (
+        <Container>
+            <Row>
+                <Col s={12} l={6}>
+                    <h5>Languages</h5>
+                    <div className="card-wrapper">
+                        { languages.map(item => <Card data={ item } />) }
+                    </div>
+                </Col>
+                <Col s={12} l={6}>
+                    <h5>Libraries</h5>
+                    <div className="card-wrapper">
+                        { libraries.map(item => <Card data={ item } />) }
+                    </div>
+                </Col>
+                <Col s={12}>
+                    <h5>Other skills</h5>
+                    <div className="card-wrapper">
+                        { others.map(item => <Card data={ item } />) }
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,59 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet"
-import { Container, Row, Col } from "react-materialize";
-import { useStaticQuery, graphql } from "gatsby";
 
 import Layout from "../components/layout"
-import Card from "../components/card"
+import Skills from "../components/skills"
 
 export default () => {
-	const data = useStaticQuery(graphql`
-		query {
-			site {
-				siteMetadata {
-					technologies {
-						name
-						type
-						image
-					}
-				}
-			}
-		}
-	`);
-	
-	const technologies = data.site.siteMetadata.technologies;
-	const languages = technologies.filter(item => item.type === 'language');
-	const libraries = technologies.filter(item => item.type === 'library');
-	const others = technologies.filter(item => item.type === 'other');
-
 	return (
 		<Layout>
 			<Helmet>
 				<title>Dan Mathisen - Full-stack web application developer</title>
 			</Helmet>
 
-			<Container>
-				<Row>
-					<Col s={12} l={6}>
-						<h5>Languages</h5>
-						<div className="card-wrapper">
-							{ languages.map(item => <Card data={ item } />) }
-						</div>
-					</Col>
-					<Col s={12} l={6}>
-						<h5>Libraries</h5>
-						<div className="card-wrapper">
-							{ libraries.map(item => <Card data={ item } />) }
-						</div>
-					</Col>
-					<Col s={12}>
-						<h5>Other skills</h5>
-						<div className="card-wrapper">
-							{ others.map(item => <Card data={ item } />) }
-						</div>
-					</Col>
-				</Row>
-			</Container>
+			<Skills />
 		</Layout>
 	)
 }

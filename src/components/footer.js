b4773f74c8de6baa8fb60050e3176d3b8ca6389d
dmathisen <dan.mathisen1@gmail.com>
2020-01-27 16:08:12 -0500

    Footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,35 @@
-import React from "react"
+import React from 'react';
+import { useStaticQuery, graphql } from "gatsby";
+import { Container, Row, Col } from "react-materialize";
+
+import './footer.css';
 
 export default () => {
+	const data = useStaticQuery(graphql`
+		query {
+			site {
+				siteMetadata {
+					title
+					subTitle
+				}
+			}
+		}
+	`);
+
 	const currentYear = (new Date()).getFullYear();
 
 	return (
 		<footer>
-			Copyright &copy; { currentYear } Dan Mathisen
+			<Container>
+				<Row>
+					<Col s={6}>
+						<p>Copyright &copy; { currentYear } { data.site.siteMetadata.title }</p>
+					</Col>
+					<Col s={6} className="right-align">
+						<p><a href="/dan-mathisen-resume.pdf" target="_blank">Download Resume (PDF)</a></p>
+					</Col>
+				</Row>
+			</Container>
 		</footer>
 	)
 }

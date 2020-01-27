4ff324cff19150c3d799a3070a50f45b7bbf2b31
dmathisen <dan@danmathisen.com>
2020-01-26 19:12:45 -0500

    Set up quote

---
+++ src/components/quote.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './quote.css'
+
+export default () => {
+    return(
+        <blockquote className="quote">
+            As a web designer &amp; developer, I build elegant websites and create eye-catching designs that provide for a simple and intuitive user experience
+        </blockquote>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Helmet } from "react-helmet"
+import { Helmet } from "react-helmet";
 
-import Layout from "../components/layout"
-import Skills from "../components/skills"
+import Layout from "../components/layout";
+import Quote from "../components/quote";
+import Skills from "../components/skills";
 
 export default () => {
 	return (
@@ -11,6 +12,7 @@ export default () => {
 				<title>Dan Mathisen - Full-stack web application developer</title>
 			</Helmet>
 
+			<Quote />
 			<Skills />
 		</Layout>
 	)

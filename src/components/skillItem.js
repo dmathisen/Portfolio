dd9c810d2371d9dc55f5962441b5ce0ff2acbb33
dmathisen <dan@danmathisen.com>
2020-01-26 22:40:47 -0500

    Fix warning message

--- src/components/skillItem.js
+++ src/components/skillItem.js
@@ -4,7 +4,7 @@ export default ({data}) => {
     const imageUrl = `/images/technologies/${data.image}`;
 
     return (
-        <div className="skill-item">
+        <div className="skill-item" key={data.name}>
             <img src={imageUrl} alt={data.name} />
             <p className="skill-item-title">{data.name}</p>
         </div>

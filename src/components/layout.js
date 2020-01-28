c15b252d0d64d4af44707469feb1ffb3da9f9f02
dmathisen <dan@danmathisen.com>
2020-01-27 20:52:33 -0500

    Fix materialize css references

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,27 +5,15 @@ import Footer from "./footer";
 import Navbar from "./navbar";
 import Waves from "./waves";
 
+import "materialize-css/dist/js/materialize.min.js";
+import "materialize-css/dist/css/materialize.min.css";
 import "./layout.css"
 
 export default ({ children }) => {
-	// handle case when these is no window/document defined (when running build)
-	const clientIsBrowser = typeof window === "object" ? true : false;
-
 	return (
 		<>
 			<Helmet>
-				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" />
 				<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-				<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-				<script>
-					{
-						// initialize Materialize navbar
-						clientIsBrowser && document.addEventListener('DOMContentLoaded', function() {
-							const sidenav = document.querySelector('.sidenav');
-							sidenav.sidenav();
-						})
-					}
-				</script>
 			</Helmet>
 
 			<header>

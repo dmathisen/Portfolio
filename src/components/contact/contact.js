9130b278cfbe09799b83f81d35e1160764da2ea9
dmathisen <dan.mathisen1@gmail.com>
2020-01-29 13:02:29 -0500

    Add contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useStaticQuery, graphql } from "gatsby";
-import { Container, Row, Col } from 'react-materialize';
+import { Container, Row, Col, Button, Icon, Preloader } from 'react-materialize';
+import M from 'materialize-css/dist/js/materialize';
 
 import './contact.css'
 
 export default() => {
+	const [ emailIsSending, setEmailIsSending ] = useState(false);
+
 	const data = useStaticQuery(graphql`
 		query {
 			site {
@@ -21,27 +24,82 @@ export default() => {
 
 	const contacts = data.site.siteMetadata.contact;
 
+	const sendEmail = e => {
+		e.preventDefault();
+
+		setEmailIsSending(true);
+		let formData = new FormData(e.target);
+
+		fetch('/mailer.php', {
+			method: 'post',
+			body: formData
+		}).then(response => {
+			return response.text();
+		}).then(response => {
+			setEmailIsSending(false);
+			if (response === "success") {
+				M.toast({ html: 'Email sent!', classes: "success" })
+			} else {
+				M.toast({ html: 'Email send failed :(', classes: "fail" })
+			}
+		}).catch(error => {
+			setEmailIsSending(false);
+			M.toast({ html: 'Email send failed :(', classes: "fail" })
+			console.error('Email send failed', error);
+		});
+	}
+
 	return (
 		<Container id="contact">
 			<Row>
-				<Col s={12}>
+				<Col s={12} className="center">
 					<h5>Find Me</h5>
+					{/* icons from https://www.iconfinder.com/iconsets/logos-and-brands */}
 
-					<div className="contact-wrapper">
+					{
+						contacts.map(contact => {
+							const imageUrl = `/images/social/${contact.image}`;
+							return (
+								<a href={contact.url} key={contact.name} className="contact-item" target="_blank" rel="noopener noreferrer">
+									<img src={imageUrl} alt={contact.name} />
+									<p>{contact.name}</p>
+								</a>
+							)
+						})
+					}
+					
+					<h5>Contact Me</h5>
+					<form onSubmit={sendEmail} id="emailForm">
 
-						{/* icons from https://www.iconfinder.com/iconsets/logos-and-brands */}
-						{
-							contacts.map(contact => {
-								const imageUrl = `/images/social/${contact.image}`;
-								return (
-									<a href={contact.url} className="contact-item" target="_blank" rel="noopener noreferrer">
-										<img src={imageUrl} alt={contact.name} />
-										<p>{contact.name}</p>
-									</a>
-								)
-							})
-						}
-					</div>
+						<div className="input-field col">
+							<i className="material-icons prefix">person</i>
+							<input type="text" id="name" name="name" />
+							<label htmlFor="name">Your name</label>
+						</div>
+
+						<div className="input-field col">
+							<i className="material-icons prefix">email</i>
+							<input className="validate" type="email" id="email" name="email" />
+							<label htmlFor="email">Email</label>
+						</div>
+						
+						<div className="input-field col">
+							<i className="material-icons prefix">message</i>
+							<textarea id="message" name="message" className="materialize-textarea"></textarea>
+							<label htmlFor="message">Your message</label>
+						</div>
+
+						{!emailIsSending && (
+							<Button node="button" type="submit" waves="light">
+								Send
+								<Icon right>send</Icon>
+							</Button>
+						)}
+
+						{emailIsSending && (
+							<Preloader color="green" size="small" />
+						)}
+					</form>
 				</Col>
 			</Row>
 		</Container>
---
+++ static/mailer.php
@@ -0,0 +1,49 @@
+<?php
+
+	header('Content-Type: application/json');
+
+	function spamCheck($field)
+	{
+		$field = filter_var($field, FILTER_SANITIZE_EMAIL);
+		if(filter_var($field, FILTER_VALIDATE_EMAIL))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	function sendMail($toEmail, $fromEmail, $subject, $message, $headers)
+	{
+		$validFromEmail = spamCheck($fromEmail);
+		if($validFromEmail)
+		{
+			mail($toEmail, $subject, $message, $headers);
+			echo "success";
+		} else {
+			echo "fail";
+		}
+		exit;
+	}
+
+	$name = isset($_POST['name']) ? $_POST['name'] : false;
+	$email = isset($_POST['email']) ? $_POST['email'] : false;
+	$message = isset($_POST['message']) ? $_POST['message'] : false;
+
+	if($name != false && $email != false && $message != false)
+	{
+		$to = "dan.mathisen1@gmail.com";
+		$subject = "Message from danmathisen.com";
+		$msg = "From: $name ($email)\n\n$message";
+
+		$headers  = "Reply-To: $name <$email>\r\n";
+		$headers .= "Return-Path: $name <$email>\r\n";
+		$headers .= "From: $name <$email>\r\n";
+		$headers .= "X-Mailer: PHP/" . phpversion();
+
+		sendMail($to, $email, $subject, $msg, $headers);
+	}
+
+?>

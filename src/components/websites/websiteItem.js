df6cc5b1fd89f9d8e538b9a4347afecbaf323784
dmathisen <dan.mathisen1@gmail.com>
2020-01-29 18:09:31 -0500

    Add fun messages when clicking portfolio website link

--- src/components/websites/websiteItem.js
+++ src/components/websites/websiteItem.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { Card, CardTitle, Chip, Button } from "react-materialize"
+import { Card, CardTitle, Chip, Button } from "react-materialize";
+import M from 'materialize-css';
 
 export default ({website, technologies}) => {
-    const imageUrl = `/images/websites/${website.image}`;
+	const imageUrl = `/images/websites/${website.image}`;
+
+	const messages = [
+		'Error: Maximum call stack exceeded.',
+		'To infinity and beyond!',
+		'This is very meta.',
+		'We seem to be going in circles.',
+		'We\'ll just stay where we are.',
+		'We\'re stuck here...',
+		'Error 429: Too many redirects.',
+		'But... you\'re already here!',
+		'This is awkward...'
+	]
+
+	const navigate = e => {
+		if (e.target.href === "https://danmathisen.com/") {
+			e.preventDefault();
+
+			const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+			M.toast({ html: randomMessage, classes: "fail" });
+		}
+	}
 
     return (
         <Card
             className="website-item"
             header={<CardTitle image={imageUrl}></CardTitle>}
             actions={[
-                <Button small href={website.website} node="a" waves="light" target="_blank" key={website.name}>Visit Website</Button>
+                <Button small href={website.website} onClick={navigate} node="a" waves="light" target="_blank" key={website.name}>Visit Website</Button>
             ]}
         >
             <h5>{website.name}</h5>

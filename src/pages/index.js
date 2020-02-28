26a531e0eeccf231fc34317d5b304228167827ad
dmathisen <dan.mathisen1@gmail.com>
2020-02-28 12:06:59 -0500

    Og image needs to be an absolute URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default () => {
         <title>Dan Mathisen - Full-stack web application developer</title>
 		<meta property="og:title" content="Dan Mathisen - Full-stack web developer" />
 		<meta property="og:description" content="I build elegant and eye-catching websites that provide a simple and intuitive user experience" />
-		<meta property="og:image" content="/android-chrome-512x512.png" />
+		<meta property="og:image" content="https://danmathisen.com/android-chrome-512x512.png" />
 		<meta property="og:url" content="https://danmathisen.com" />
       </Helmet>
 

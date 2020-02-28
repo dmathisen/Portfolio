9a9ebf5fdea2618c1ea897b224f9c9f354da6261
dmathisen <dan.mathisen1@gmail.com>
2020-02-28 11:52:24 -0500

    Add favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,9 +11,13 @@ export default ({ children }) => {
   return (
     <>
       <Helmet>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+		  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+		  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+		  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+		  
+		  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"></link>
+		  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"></link>
+		  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
       </Helmet>
 
       <header id="top">
